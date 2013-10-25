f95c5a0e08257f503df0f1307ea6aaf0587f6201
dikonikon <dikonikon@gmail.com>
2013-10-25 10:29:17 +0100

    compound build files

--- server/project/Build.scala
+++ server/project/Build.scala
@@ -4,14 +4,15 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "webserver"
+  val appName         = "server"
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
     jdbc,
     anorm,
-    "com.typesafe.akka" %% "akka-actor" % "2.3-SNAPSHOT"
+    "com.typesafe.akka" %% "akka-actor" % "2.1.0",
+	"common" %% "common" % "1.0-SNAPSHOT"
   )
 
 
